ae43eb13520c2397a23a3c1e3ee25bce81f407c1
Hugh Brennan <hugh.o.brenan@gmail.com>
2024-09-16 18:57:45 +0100

    changes

--- app/match_viewer/[id]/page.tsx
+++ app/match_viewer/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React, { useState } from 'react';
 import { Typography, Container, Box, Grid, Paper } from '@mui/material';
--- app/upload_csv/page.tsx
+++ app/upload_csv/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React, { useState } from 'react';
 import { Button, Typography, Box, CircularProgress, Paper, Snackbar } from '@mui/material';
